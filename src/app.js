7021568ec03dbd8b16a5762b15109b712eaef66b
Sebastian Morales <fono.smc@gmail.com>
2022-07-12 02:53:08 -0400

    Agregada dependencia cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import productRoutes from "./routes/product.routes.js";
 import config from "./config.js";
+import cors from "cors";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.set("port", config.port);
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use("/api/products", productRoutes);
 
