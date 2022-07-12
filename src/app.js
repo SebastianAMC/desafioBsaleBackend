61f124295aa0ea57ae5fda07ef4e49a8c0a7d243
Sebastian Morales <fono.smc@gmail.com>
2022-07-12 00:42:38 -0400

    Modificación para deploy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import productRoutes from "./routes/product.routes.js";
+import config from "./config.js";
 
 const app = express();
 
 // Settings
-app.set("port", 4000);
+app.set("port", config.port);
 
 // Middlewares
 app.use(morgan("dev"));
--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ config();
 
 export default {
     host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 4000,
     database: process.env.DATABASE || "",
     user: process.env.USER || "",
     password: process.env.PASSWORD || "",
