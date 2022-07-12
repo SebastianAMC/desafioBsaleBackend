e52e15b4861e477012e75814565d972efdf64b8d
Sebastian Morales <fono.smc@gmail.com>
2022-07-12 00:30:17 -0400

    Modificación para deploy

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-    host: process.env.HOST || 4000,
+    host: process.env.HOST || '0.0.0.0',
     database: process.env.DATABASE || "",
     user: process.env.USER || "",
     password: process.env.PASSWORD || "",
